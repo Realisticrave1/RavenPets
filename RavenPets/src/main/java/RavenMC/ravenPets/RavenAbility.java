3ad9230cc2481dd3ed3fc19b8f15378dacfddea0
Realisticrave1 <matthew@voidlands.org>
2025-05-05 13:15:00 -0400

    Delete RavenPets directory

--- RavenPets/src/main/java/RavenMC/ravenPets/RavenAbility.java
+++
@@ -1,9 +0,0 @@
-package RavenMC.ravenPets;
-
-import org.bukkit.entity.Player;
-
-public interface RavenAbility {
-    void execute(Player player, RavenTier tier);
-    String getName();
-    String getDescription();
-}
